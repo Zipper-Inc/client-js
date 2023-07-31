ba432266c423fa74364cd07cfea98aea7bdaa7e8
Ibrahim Madha <imadha@users.noreply.github.com>
2023-07-31 15:32:39 -0500

    Upgrade DTN and gzip dist directory (#2)
    * Import newer version of DTN
    
    * Fix editor setting
    
    * Remove extra package.json work $ build a dist tar
    
    * Bump version

--- bin/build.ts
+++ bin/build.ts
@@ -2,7 +2,7 @@
 import {
   build as buildNodePackage,
   emptyDir,
-} from 'https://deno.land/x/dnt@0.32.0/mod.ts';
+} from 'https://deno.land/x/dnt@0.38.0/mod.ts';
 import pkgJson from '../package.json' assert { type: 'json' };
 
 const { name, version, description, author, license, repository, bugs } =
@@ -11,33 +11,6 @@ const { name, version, description, author, license, repository, bugs } =
 export const SRC_DIR = './src';
 export const OUT_DIR = './dist';
 export const ENTRY_POINT = `${SRC_DIR}/index.ts`;
-const OUT_PACKAGE_JSON_PATH = `${OUT_DIR}/package.json`;
-
-/**
- * Enforces known `package.json` order
- * Fixes the `Module not found: Default condition should be last one` error
- * @param path Path to output package.json
- */
-async function enforcePkgJsonOrder(path: string) {
-  const { default: outPkgJson } = await import(`../${path}`, {
-    assert: { type: 'json' },
-  });
-
-  const pkgImport = outPkgJson.exports['.'].import;
-  const pkgRequire = outPkgJson.exports['.'].require;
-
-  outPkgJson.exports['.'].import = {
-    types: pkgImport.types,
-    default: pkgImport.default,
-  };
-
-  outPkgJson.exports['.'].require = {
-    types: pkgRequire.types,
-    default: pkgRequire.default,
-  };
-
-  return Deno.writeTextFile(path, JSON.stringify(outPkgJson, undefined, 2));
-}
 
 export async function build() {
   await emptyDir(OUT_DIR);
@@ -61,7 +34,12 @@ export async function build() {
       // steps to run after building and before running the tests
       await Deno.copyFile('LICENSE', `${OUT_DIR}/LICENSE`);
       await Deno.copyFile('README.md', `${OUT_DIR}/README.md`);
-      await enforcePkgJsonOrder(OUT_PACKAGE_JSON_PATH);
+
+      // Tar dist
+      const cmd = new Deno.Command('tar', {
+        args: ['-czf', `dist--v${version}.tar.gz`, `${OUT_DIR}/`],
+      });
+      await cmd.output();
     },
   });
 }
